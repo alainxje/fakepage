4ea93b17cf19302735b847dd5d059f1329114255
alainxje <alainnoujaim@hotmail.com>
2025-04-09 20:24:22 +0200

    test4

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def login():
 
 if __name__ == '__main__':
     # Use the PORT environment variable or fallback to 5000 for local development
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+
+    app.run(host='0.0.0.0', port=80, debug=True)

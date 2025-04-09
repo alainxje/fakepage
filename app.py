89476f1e8fd8589e22de8a06bf2f0e4d0c9dc5ce
alainxje <alainnoujaim@hotmail.com>
2025-04-09 20:22:38 +0200

    test3

--- app.py
+++ app.py
@@ -41,4 +41,6 @@ def login():
     return jsonify({'status': 'error', 'message': 'Please try again'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable or fallback to 5000 for local development
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
